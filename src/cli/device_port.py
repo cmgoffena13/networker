428e75915d7e2f1be13a2632bc243f13a3f323ee
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2025-12-23 13:32:38 -0700

    typo

--- src/cli/device_port.py
+++ src/cli/device_port.py
@@ -8,7 +8,7 @@ from src.logging_conf import set_log_level
 
 logger = structlog.getLogger(__name__)
 
-device_port_typer = Typer(help="Open port commands")
+device_port_typer = Typer(help="Open Port commands")
 
 
 @device_port_typer.command("list", help="List all open ports for a device")
@@ -25,7 +25,7 @@ def list(
         device_ports = db_list_device_ports(device_id)
         device_name = device.device_name or "Unknown"
         echo(
-            f"Listing the {len(device_ports)} open ports for device (MAC: {device.device_mac}, Name: {device_name})..."
+            f"Listing the {len(device_ports)} open ports for device (MAC: {device.device_mac}, Name: {device_name}, ID: {device.id})..."
         )
         for device_port, service_name, description in device_ports:
             echo(display_port_info(device_port, service_name, description))
