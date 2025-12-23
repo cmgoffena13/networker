2aa79d2eeb169a924efe4dd49a015e7c8873eac0
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2025-12-22 22:41:36 -0700

    small changes

--- src/cli/device.py
+++ src/cli/device.py
@@ -1,13 +1,11 @@
 from typer import Typer
 
-from src.cli.console import echo
-
 device = Typer(help="Device commands")
 
 
 @device.command("scan", help="Scan the device for open ports")
 def scan():
-    echo(f"Scanning device...")
+    pass
 
 
 @device.command("list", help="list information on devices stored")
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,2 +1,6 @@
 class NetworkNotFoundError(Exception):
     pass
+
+
+class DeviceNotFoundError(Exception):
+    pass
