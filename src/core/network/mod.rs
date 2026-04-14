a7e75fa4ee9c452f9b83bb89fb3510b1eba583e8
Cortland Goffena <30168413+cmgoffena13@users.noreply.github.com>
2026-04-13 21:15:38 -0600

    rename detect_utils to network

--- src/core/network/mod.rs
+++ src/core/network/mod.rs
@@ -1,4 +1,4 @@
-pub mod detect_utils;
+pub mod network;
 pub mod mdns;
 
-pub use detect_utils::{get_local_network, iter_ipv4_ping_targets};
+pub use self::network::{get_local_network, iter_ipv4_ping_targets};
